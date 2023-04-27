37811b81e443026b3aab482c4f37e8a46bfd4f13
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-04-27 12:16:07 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1 className="Title">Welcome to our site</h1>
+      <li>
+      <Link className="view-links" to="/actors">View Actors</Link>
+      </li>
+      <li>
+      <Link className="view-links" to="/directors">View Directors</Link>
+      </li>
+      <li>
+      <Link className="view-links" to="/movies">View Movies</Link>
+      </li>
+    </div>
+  )
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <nav>
+    <ul>
+      <li>
+        <NavLink className="nav-link" to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink className="nav-link" to="/actors">Actors</NavLink>
+      </li>
+      <li>
+        <NavLink className="nav-link" to="/directors">Directors</NavLink>
+      </li>
+      <li>
+        <NavLink className="nav-link" to="/movies">Movies</NavLink>
+      </li>
+    </ul>
+  </nav>
+    )
 }
 
 export default NavBar;
